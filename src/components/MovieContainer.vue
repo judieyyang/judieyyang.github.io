f8c14d7ee1682848d9b129025ea269682ce0d209
Michael <openmichael65@gmail.com>
2021-04-15 23:08:28 -0700

    Fix css

--- src/components/MovieContainer.vue
+++ src/components/MovieContainer.vue
@@ -82,6 +82,7 @@ export default {
 	margin: 20px;
 	border-radius: 20px;
 	box-shadow: 0 7px 30px -10px rgba(150, 170, 180, 0.5);
+	background-color: #FDF5E6;
 
 	display: flex;
 	flex-direction: column;
